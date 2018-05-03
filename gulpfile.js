c613ee6f3d9e1ba7f66955bada9c3ece0344bb89
Chris <capozzic1@gmail.com>
2018-05-03 10:55:43 -0400

    Add added in gulp, created html, sass partial partials

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('./stylesheets/main.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./public'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./stylesheets/**/*.scss', ['sass']);
+});
